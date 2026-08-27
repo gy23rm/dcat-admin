b8942f711bea795357c6ff532e2cbb1995c739a4
zgy <gy23rm@163.com>
2026-08-27 11:41:28 +0800

    修复  语法 错误

--- resources/views/form/multipleselect.blade.php
+++ resources/views/form/multipleselect.blade.php
@@ -23,7 +23,7 @@
 @include('admin::form.select-script')
 
 <script>
-$('.{{$selector}}').off('change.select2').on('change.select2', function () {
+$('{{$selector}}').off('change.select2').on('change.select2', function () {
     $(this).closest('.field').find('input[type="hidden"][name="{{$name}}[]"]').prop('disabled', $(this).val()?.length > 0);
 });
 </script>
